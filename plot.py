e1bd121302c717675f8e7742468b5b649ef9eb50
YRC10 <yaoruichen0224@gmail.com>
2022-10-11 00:44:02 -0700

    add plot py

---
+++ plot.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+df = pd.read_csv('./CPEN502/BinaryNoMomentum.csv','r')
+
+i = 0
+totalepoch = df.shape[0]
+epoch = []
+loss = []
+print(totalepoch)
+
+for i in range (totalepoch):
+    loss.append(df.iat[i, 0])
+    epoch.append(i)
+
+plt.figure(figsize=(8, 6))
+
+plt.plot(epoch, loss)
+
+plt.xlabel('Epochs')
+plt.ylabel('Training Looses')
+plt.title('Training Losses VS Epochs')
+plt.show()
